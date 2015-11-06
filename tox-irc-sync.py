34e1af3497951062d991fbd1abdbd81e7068de20
quininer <quininer@live.com>
2015-11-06 13:10:31 +0800

    remove av

--- tox-irc-sync.py
+++ tox-irc-sync.py
@@ -6,7 +6,7 @@ import re
 import pickle
 import ssl
 
-from pytox import Tox, ToxAV, OperationFailedError
+from pytox import Tox, OperationFailedError
 
 from time import sleep
 from os.path import exists
@@ -25,53 +25,10 @@ TOX_NAME = NAME = NICK = IDENT = REALNAME = 'toxsync-test'
 CHANNEL = '#linux-cn-test'
 MEMORY_DB = 'memory.pickle'
 
-class AV(ToxAV):
-    def __init__(self, core, max_calls):
-        self.core = self.get_tox()
-        self.cs = None
-        self.call_type = self.TypeAudio
-
-    def on_invite(self, idx):
-        self.cs = self.get_peer_csettings(idx, 0)
-        self.call_type = self.cs['call_type']
-
-        print('Incoming %s call from %d:%s ...' % (
-                'video' if self.call_type == self.TypeVideo else 'audio', idx,
-                self.core.get_name(self.get_peer_id(idx, 0))))
-
-        self.answer(idx, self.call_type)
-        print('Answered, in call...')
-
-    def on_start(self, idx):
-        self.change_settings(idx, {'max_video_width': 1920,
-                                   'max_video_height': 1080})
-        self.prepare_transmission(idx, self.jbufdc * 2, self.VADd,
-                True if self.call_type == self.TypeVideo else False)
-
-    def on_end(self, idx):
-        self.kill_transmission()
-
-        print('Call ended')
-
-    def on_peer_timeout(self, idx):
-        self.stop_call()
-
-    def on_audio_data(self, idx, size, data):
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        self.send_audio(idx, size, data)
-
-    def on_video_data(self, idx, width, height, data):
-        sys.stdout.write('*')
-        sys.stdout.flush()
-        self.send_video(idx, width, height, data)
 
 class SyncBot(Tox):
     def __init__(self):
-        if exists('data'):
-            self.load_from_file('data')
 
-        self.av = AV(self, 10)
         self.connect()
         self.self_set_name(TOX_NAME)
         self.self_set_status_message("Send me a message with the word 'invite'")
